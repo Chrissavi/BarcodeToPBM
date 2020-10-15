cd72c48b70d9326f48f402d9a22b754531e4156e
Christian Savinkin <christian.savinkin@gmx.de>
2020-10-15 18:57:23 +0200

    Add project files.

---
+++ MicroConsoleBarcode/MicroConsoleBarcode.cpp
@@ -0,0 +1,32 @@
+﻿#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+string code;
+void CalculateBarcode();
+
+int main()
+{
+    cin >> code;
+    CalculateBarcode();
+    return 0;
+}
+void CalculateBarcode()
+{
+    int lenght = code.size();
+    int width = lenght;
+    int height = 30;
+        
+    ofstream barcode(code + ".pbm");
+    barcode << "P1" << endl;
+    barcode << width << " " << height << endl;
+    
+    for (int i = 0; i < 31; i++)
+    {
+        barcode << code << endl;
+    }
+        
+}
+
+
