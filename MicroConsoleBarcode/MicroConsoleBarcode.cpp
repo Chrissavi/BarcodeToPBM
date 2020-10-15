ad4ba941bbb98b25903d8104c2177eec79720dad
Christian Savinkin <christian.savinkin@gmx.de>
2020-10-15 19:20:46 +0200

    changed a redundant variable and added screenshot

--- MicroConsoleBarcode/MicroConsoleBarcode.cpp
+++ MicroConsoleBarcode/MicroConsoleBarcode.cpp
@@ -8,21 +8,22 @@ void CalculateBarcode();
 
 int main()
 {
+    // Please use for now only "1"s and "0"s. -- 1 is black | 0 is white
     cin >> code;
     CalculateBarcode();
     return 0;
 }
 void CalculateBarcode()
 {
-    int lenght = code.size();
-    int width = lenght;
+    int width = code.size();
     int height = 30;
         
     ofstream barcode(code + ".pbm");
     barcode << "P1" << endl;
     barcode << width << " " << height << endl;
     
-    for (int i = 0; i < 31; i++)
+    // multiply code to get human-readable height
+    for (int i = 0; i < width + 1; i++)
     {
         barcode << code << endl;
     }
