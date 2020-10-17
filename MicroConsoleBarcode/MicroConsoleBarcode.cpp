151abbdcda18aae0930976799979b74b48d9e8e6
Christian Savinkin <christian.savinkin@gmx.de>
2020-10-17 11:34:44 +0200

    Upload new branch for EAN

--- MicroConsoleBarcode/MicroConsoleBarcode.cpp
+++ MicroConsoleBarcode/MicroConsoleBarcode.cpp
@@ -1,33 +1,98 @@
-﻿#include <fstream>
-#include <iostream>
-#include <string>
-using namespace std;
+﻿#include "MicroConsoleBarcode.h"
 
-string code;
-void CalculateBarcode();
 
 int main()
 {
-    // Please use for now only "1"s and "0"s. -- 1 is black | 0 is white
-    cin >> code;
-    CalculateBarcode();
+    cin >> Usercode;
+    cout << Usercode << endl;
+    strcpy_s(InternalCode, Usercode.c_str());
+
+    for (int i = 0; i < 14; i++)
+        cout << InternalCode[i];
+
+    cout << InternalCode[0] << endl;
+    cout << InternalCode[1] << endl;
+    cout << InternalCode[2] << endl;
+    cout << InternalCode[3] << endl;
+    cout << InternalCode[4] << endl;
+    cout << InternalCode[5] << endl;
+    cout << InternalCode[6] << endl;
+    cout << InternalCode[7] << endl;
+
+
+
+    /*
+    for (int i = 0; i < 3; i++)
+    {
+        std::cin >> Array;
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        cout << Array << endl;
+    }
+    */
+    //CalculateBarcode();
     return 0;
 }
+
+void ExtractCode()
+{
+    for (int i = 0; i < 14; i++)
+    {
+        switch (InternalCode[i])
+        {
+        case(0):
+            break;
+        case(1):
+            break;
+        case(2):
+            break;
+        case(3):
+            break;
+        case(4):
+            break;
+        case(5):
+            break;
+        case(6):
+            break;
+        case(7):
+            break;
+        case(8):
+            break;
+        case(9):
+            break;
+        }
+    }    
+    
+}
+/*
 void CalculateBarcode()
 {
-    int width = code.size();
+    int width = 13;
     int height = 30;
-        
-    ofstream barcode(code + ".pbm");
+    
+    for (int i = 0; i < 14; i++)
+    {
+        switch (Array[i])
+        {
+        case(Array[0]):
+            break;
+        }
+            
+
+    }
+
+
+    ofstream barcode("test.pbm");
     barcode << "P1" << endl;
     barcode << width << " " << height << endl;
     
     // multiplies code to get human-readable height
     for (int i = 0; i < width + 1; i++)
     {
-        barcode << code << endl;
+        barcode << Array << endl;
     }
         
 }
-
+*/
 
---
+++ MicroConsoleBarcode/MicroConsoleBarcode.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+string Usercode;
+
+char InternalCode[14];
+
+void CalculateBarcode();
