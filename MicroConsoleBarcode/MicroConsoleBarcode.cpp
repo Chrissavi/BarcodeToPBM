21eee2e6b3341d4b88202e7a6d1d7564c2b28073
Christian Savinkin <christian.savinkin@gmx.de>
2020-10-17 17:47:55 +0200

    Internal logic buildup

--- MicroConsoleBarcode/MicroConsoleBarcode.cpp
+++ MicroConsoleBarcode/MicroConsoleBarcode.cpp
@@ -40,31 +40,191 @@ void ExtractCode()
 
     switch (InternalCode[0])
     {
-    case(0):
+    case(0): 
+        strcpy_s(STYLE, ZERO[4].c_str());
+        GeneratedInternalCode[0] = ZERO[0];
         break;
-    case(1):
+    case(1): 
+        strcpy_s(STYLE, ONE[4].c_str());
+        GeneratedInternalCode[0] = ONE[0];
         break;
-    case(2):
+    case(2): 
+        strcpy_s(STYLE, TWO[4].c_str());
+        GeneratedInternalCode[0] = TWO[0];
         break;
-    case(3):
+    case(3): 
+        strcpy_s(STYLE, THREE[4].c_str());
+        GeneratedInternalCode[0] = THREE[0];
         break;
-    case(4):
+    case(4): 
+        strcpy_s(STYLE, FOUR[4].c_str());
+        GeneratedInternalCode[0] = FOUR[0];
         break;
-    case(5):
+    case(5): 
+        strcpy_s(STYLE, FIVE[4].c_str());
+        GeneratedInternalCode[0] = FIVE[0];
         break;
-    case(6):
+    case(6): 
+        strcpy_s(STYLE, SIX[4].c_str());
+        GeneratedInternalCode[0] = SIX[0];
         break;
-    case(7):
+    case(7): 
+        strcpy_s(STYLE, SEVEN[4].c_str());
+        GeneratedInternalCode[0] = SEVEN[0];
         break;
-    case(8):
+    case(8): 
+        strcpy_s(STYLE, EIGHT[4].c_str());
+        GeneratedInternalCode[0] = EIGHT[0];
         break;
-    case(9):
+    case(9): 
+        strcpy_s(STYLE, NINE[4].c_str());
+        GeneratedInternalCode[0] = NINE[0];
         break;
     }
 
-    for (int i = 0; i < 14; i++)
+    for (int i = 7; i < 14; i++)
     {
-        
+        switch (InternalCode[i])
+        {
+        case(0): 
+            GeneratedInternalCode[i] = ZERO[3];
+            break;
+        case(1):
+            GeneratedInternalCode[i] = ONE[3];
+            break;
+        case(2):
+            GeneratedInternalCode[i] = TWO[3];
+            break;
+        case(3):
+            GeneratedInternalCode[i] = THREE[3];
+            break;
+        case(4):
+            GeneratedInternalCode[i] = FOUR[3];
+            break;
+        case(5):
+            GeneratedInternalCode[i] = FIVE[3];
+            break;
+        case(6):
+            GeneratedInternalCode[i] = SIX[3];
+            break;
+        case(7):
+            GeneratedInternalCode[i] = SEVEN[3];
+            break;
+        case(8):
+            GeneratedInternalCode[i] = EIGHT[3];
+            break;
+        case(9):
+            GeneratedInternalCode[i] = NINE[3];
+            break;
+        }
+    }
+
+    for (int i = 1; i < 7; i++)
+    {
+        char CurrentStyle = STYLE[i];
+
+        switch (InternalCode[i])
+        {
+        case(0): 
+            if (CurrentStyle == 0)
+        {
+            GeneratedInternalCode[i] = ZERO[0];
+        }
+        else
+        {
+            GeneratedInternalCode[i] = ZERO[1];
+        }
+            break;
+        case(1): 
+            if (CurrentStyle == 0)
+            {
+                GeneratedInternalCode[i] = ONE[0];
+            }
+            else
+            {
+                GeneratedInternalCode[i] = ONE[1];
+            }
+            break;
+        case(2): 
+            if (CurrentStyle == 0)
+            {
+                GeneratedInternalCode[i] = TWO[0];
+            }
+            else
+            {
+                GeneratedInternalCode[i] = TWO[1];
+            }
+            break;
+        case(3): 
+            if (CurrentStyle == 0)
+            {
+                GeneratedInternalCode[i] = THREE[0];
+            }
+            else
+            {
+                GeneratedInternalCode[i] = THREE[1];
+            }
+            break;
+        case(4): 
+            if (CurrentStyle == 0)
+            {
+                GeneratedInternalCode[i] = FOUR[0];
+            }
+            else
+            {
+                GeneratedInternalCode[i] = FOUR[1];
+            }
+            break;
+        case(5): 
+            if (CurrentStyle == 0)
+            {
+                GeneratedInternalCode[i] = FIVE[0];
+            }
+            else
+            {
+                GeneratedInternalCode[i] = FIVE[1];
+            }
+            break;
+        case(6): 
+            if (CurrentStyle == 0)
+            {
+                GeneratedInternalCode[i] = SIX[0];
+            }
+            else
+            {
+                GeneratedInternalCode[i] = SIX[1];
+            }
+            break;
+        case(7): 
+            if (CurrentStyle == 0)
+            {
+                GeneratedInternalCode[i] = SEVEN[0];
+            }
+            else
+            {
+                GeneratedInternalCode[i] = SEVEN[1];
+            }
+            break;
+        case(8): 
+            if (CurrentStyle == 0)
+            {
+                GeneratedInternalCode[i] = EIGHT[0];
+            }
+            else
+            {
+                GeneratedInternalCode[i] = EIGHT[1];
+            }
+            break;
+        case(9): 
+            if (CurrentStyle == 0)
+            {
+                GeneratedInternalCode[i] = NINE[0];
+            }
+            else
+            {
+                GeneratedInternalCode[i] = NINE[1];
+            }
+            break;
     }    
     
 }
--- MicroConsoleBarcode/MicroConsoleBarcode.h
+++ MicroConsoleBarcode/MicroConsoleBarcode.h
@@ -19,8 +19,10 @@ string SEVEN[4] = { "0111011", "0010001", "1000100", "010101" };
 string EIGHT[4] = { "0110111", "0001001", "1001000", "010110" };
 string NINE[4]	= { "0001011", "0010111", "1110100", "011010" };
 
-int STYLE[6];
+char STYLE[6];
 string LRMARK	= {"101"};
 string MMARK	= {"01010"};
+
+string GeneratedInternalCode[13];
 
 void CalculateBarcode();
