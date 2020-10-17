5ed73281b092dd2fd952fdd3c3c2e3cdc0c4bf33
Christian Savinkin <christian.savinkin@gmx.de>
2020-10-17 14:10:36 +0200

    Added Binary Colorcodes

--- MicroConsoleBarcode/MicroConsoleBarcode.cpp
+++ MicroConsoleBarcode/MicroConsoleBarcode.cpp
@@ -37,31 +37,34 @@ int main()
 
 void ExtractCode()
 {
+
+    switch (InternalCode[0])
+    {
+    case(0):
+        break;
+    case(1):
+        break;
+    case(2):
+        break;
+    case(3):
+        break;
+    case(4):
+        break;
+    case(5):
+        break;
+    case(6):
+        break;
+    case(7):
+        break;
+    case(8):
+        break;
+    case(9):
+        break;
+    }
+
     for (int i = 0; i < 14; i++)
     {
-        switch (InternalCode[i])
-        {
-        case(0):
-            break;
-        case(1):
-            break;
-        case(2):
-            break;
-        case(3):
-            break;
-        case(4):
-            break;
-        case(5):
-            break;
-        case(6):
-            break;
-        case(7):
-            break;
-        case(8):
-            break;
-        case(9):
-            break;
-        }
+        
     }    
     
 }
